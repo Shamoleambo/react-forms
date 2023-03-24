c51fcd0bbc6e4660ebfcb244a9dca1bec0a635e9
Tiago Gomes <tidgomes@gmail.com>
2023-03-24 08:43:04 -0300

    make SimpleInput component a controlled component by adding the nameInput state

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,15 +1,34 @@
-const SimpleInput = (props) => {
+import { useState } from 'react'
+
+const SimpleInput = props => {
+  const [nameInput, setNameInput] = useState('')
+
+  const nameInputHandler = event => {
+    setNameInput(event.target.value)
+  }
+
+  const formSubmitHandler = event => {
+    event.preventDefault()
+
+    console.log(nameInput)
+  }
+
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+          type='text'
+          id='name'
+          value={nameInput}
+          onChange={nameInputHandler}
+        />
       </div>
-      <div className="form-actions">
+      <div className='form-actions'>
         <button>Submit</button>
       </div>
     </form>
-  );
-};
+  )
+}
 
-export default SimpleInput;
+export default SimpleInput
