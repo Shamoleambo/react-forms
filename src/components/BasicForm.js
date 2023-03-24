404accc5d2a8a6c31bdadc76d29002a6637c4d7e
Tiago Gomes <tidgomes@gmail.com>
2023-03-24 12:26:26 -0300

    make BasicForm a controlled component by adding states

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,25 +1,47 @@
-const BasicForm = (props) => {
+import { useState } from 'react'
+
+const BasicForm = () => {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+
+  const firstNameHandler = event => {
+    setFirstName(event.target.value)
+  }
+  const lastNameHandler = event => {
+    setLastName(event.target.value)
+  }
+  const emailChangeHandler = event => {
+    setEmail(event.target.value)
+  }
+
+  const formSubmitHandler = event => {
+    event.preventDefault()
+
+    console.log([firstName, lastName, email])
+  }
+
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className='control-group'>
         <div className='form-control'>
           <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+          <input type='text' id='name' onChange={firstNameHandler} />
         </div>
         <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+          <label htmlFor='last-name'>Last Name</label>
+          <input type='text' id='last-name' onChange={lastNameHandler} />
         </div>
       </div>
       <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+        <label htmlFor='email'>E-Mail Address</label>
+        <input type='email' id='email' onChange={emailChangeHandler} />
       </div>
       <div className='form-actions'>
         <button>Submit</button>
       </div>
     </form>
-  );
-};
+  )
+}
 
-export default BasicForm;
+export default BasicForm
